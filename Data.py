9105fdf00723d0e662aa0c3b8a78e481eb065e75
dp852 <dp852@georgetown.edu>
2022-04-17 20:35:44 -0400

    Update Data.py

--- Data.py
+++ Data.py
@@ -9,27 +9,27 @@ fields = ["id",
           ".school.locale"
           "latest.student.size",
           "location.lat",
-          #"location.lon",
-          #"location.city",
-          #"location.state",
-          #"latest.admissions.admission_rate.overall",
-          #"latest.admissions.sat_scores.midpoint.math",
-          #"latest.admissions.sat_scores.25th_percentile.math",
-          #"latest.admissions.sat_scores.75th_percentile.math",
-          #"latest.admissions.sat_scores.midpoint.critical_reading",
-          #"latest.admissions.sat_scores.25th_percentile.critical_reading",
-          #"latest.admissions.sat_scores.75th_percentile.critical_reading",
-          #"latest.admissions.act_scores.25th_percentile.cumulative",
-          #"latest.admissions.act_scores.midpoint.cumulative",
-          #"latest.admissions.act_scores.75th_percentile.cumulative",
-          #"latest.completion.title_iv.depend.completed_by.4yrs"
-          #"latest.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings"
-          #"latest.earnings.10_yrs_after_entry.working_not_enrolled.std_dev"
-          #"latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings"
-          #"latest.earnings.6_yrs_after_entry.working_not_enrolled.std_dev"
-          #"latest.earnings.8_yrs_after_entry.mean_earnings"
-          #"latest.earnings.8_yrs_after_entry.std_deviation"
-          #"latest.completion.completion_rate_4yr_100nt"
+          "location.lon",
+          "location.city",
+          "location.state",
+          "latest.admissions.admission_rate.overall",
+          "latest.admissions.sat_scores.midpoint.math",
+          "latest.admissions.sat_scores.25th_percentile.math",
+          "latest.admissions.sat_scores.75th_percentile.math",
+          "latest.admissions.sat_scores.midpoint.critical_reading",
+          "latest.admissions.sat_scores.25th_percentile.critical_reading",
+          "latest.admissions.sat_scores.75th_percentile.critical_reading",
+          "latest.admissions.act_scores.25th_percentile.cumulative",
+          "latest.admissions.act_scores.midpoint.cumulative",
+          "latest.admissions.act_scores.75th_percentile.cumulative",
+          "latest.completion.title_iv.depend.completed_by.4yrs"
+          "latest.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings"
+          "latest.earnings.10_yrs_after_entry.working_not_enrolled.std_dev"
+          "latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings"
+          "latest.earnings.6_yrs_after_entry.working_not_enrolled.std_dev"
+          "latest.earnings.8_yrs_after_entry.mean_earnings"
+          "latest.earnings.8_yrs_after_entry.std_deviation"
+          "latest.completion.completion_rate_4yr_100nt"
 
 
 
@@ -41,3 +41,13 @@ api_key = "&api_key=cZauPwdyxxKrzuVe7mM8WYOrWAQZymc9UsHhn3QV"
 request_url = base_url + dataset + filter_params + \
               "&fields=" + ",".join(fields) + options + api_key
 print(request_url[:-40])
+
+resp = requests.get(request_url)
+resp.raise_for_status()
+
+resp.json()
+college_info = pd.DataFrame(resp.json()['results'])
+college_info
+college_info.to_excel("college_info.xlsx")
+
+college_info
