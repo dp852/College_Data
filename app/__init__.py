3acbc21c0ad113cecc65b1eb35fc33506ae3700c
dp852 <dp852@georgetown.edu>
2022-04-27 17:56:30 -0400

    Work

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    #app.register_blueprint(home_routes)
+    app.register_blueprint(home_routes)
     #app.register_blueprint(book_routes)
     #app.register_blueprint(weather_routes)
     return app 
---
+++ web_app/routes/__pycache__/home_routes.py
@@ -0,0 +1,13 @@
+# web_app/routes/home_routes.py
+
+from flask import Blueprint, request, jsonify, render_template, redirect, flash
+from app.app import fetch_data
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+@home_routes.route("/home")
+def index():
+    print("HOME...")
+    #return "Welcome Home"
+    return render_template("home.html")
