cdaadb6ef454fe600257f066380ed8845189de5c
dp852 <dp852@georgetown.edu>
2022-05-02 17:19:44 -0400

    Change to dict variable

--- app/college_data.py
+++ app/college_data.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 #@app.route("/")
 
+
+
 def get_data(val):
     url = 'https://raw.githubusercontent.com/dp852/College_Data/main/Schools_data'
     df = pd.read_csv(url)
@@ -30,7 +32,7 @@ def get_data(val):
     upper_math = int(df['latest.admissions.sat_scores.75th_percentile.math'][df[df['school.name'] == val].index[0]])
     upper_verbal = int(df['latest.admissions.sat_scores.75th_percentile.critical_reading'][df[df['school.name'] == val].index[0]])
     upper_sat = upper_math + upper_verbal
-    median_act = int(df['latest.admissions.act_scores.midpoint.cumulative'][df[df['school.name'] == val].index[0]])
+    average_act = int(df['latest.admissions.act_scores.midpoint.cumulative'][df[df['school.name'] == val].index[0]])
     lower_act = int(df['latest.admissions.act_scores.25th_percentile.cumulative'][df[df['school.name'] == val].index[0]])
     upper_act = int(df['latest.admissions.act_scores.75th_percentile.cumulative'][df[df['school.name'] == val].index[0]])
     percent_white = "{:.2%}".format(df['latest.student.demographics.race_ethnicity.white'][df[df['school.name'] == val].index[0]], "%")
@@ -40,78 +42,17 @@ def get_data(val):
     acceptance_rate = "{:.2%}".format(df['latest.admissions.admission_rate.overall'][df[df['school.name'] == val].index[0]], "%")
     retention = "{:.2%}".format(df['latest.student.retention_rate.four_year.full_time'][df[df['school.name'] == val].index[0]])
     enrollment = int(df['latest.student.size'][df[df['school.name'] == val].index[0]])
+    location = df['school.locale'][df[df['school.name'] == val].index[0]]
     in_state = "${:,.2f}".format(df['latest.cost.tuition.in_state'][df[df['school.name'] == val].index[0]])
     out_state = "${:,.2f}".format(df['latest.cost.tuition.out_of_state'][df[df['school.name'] == val].index[0]])
     grad_rate = "{:.2%}".format(df['latest.completion.completion_rate_4yr_100nt'][df[df['school.name'] == val].index[0]])
-    Salary_6 = "${:,.2f}".format(df['latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings'][df[df['school.name'] == val].index[0]])
+    salary_6 = "${:,.2f}".format(df['latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings'][df[df['school.name'] == val].index[0]])
     salary_8 = "${:,.2f}".format(df['latest.earnings.8_yrs_after_entry.mean_earnings'][df[df['school.name'] == val].index[0]])
     salary_10 = "${:,.2f}".format(df['latest.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings'][df[df['school.name'] == val].index[0]])
-    return enrollment, lower_sat,average_sat
+    score = {"Average SAT": average_sat, "Average Math SAT": median_math_sat, "Average Verbal SAT": median_verbal_sat, "Average ACT": average_act,
+             "Acceptance Rate": acceptance_rate, "Retention Rate": retention, "Undergraduate Enrollment": enrollment, "Location": location, 
+             "Percent White": percent_white, "Percent Black": percent_black, "Percent Asian": percent_asian, "Percdent Hispanic": percent_hispanic,
+             "In-state Tuition": in_state, "Out-of-state Tuition": out_state, "Graduation Rate": grad_rate, "Salary 6-years Post Entry": salary_6, 
+             "Salary 8-years Post Entry": salary_8, "Salary 10-years Post-Entry": salary_10}
+    return score
 
-
-'''''
-def fetch_data():
-    url = 'https://raw.githubusercontent.com/dp852/College_Data/main/Schools_data'
-    df = pd.read_csv(url)
-    df["school.name"] = df["school.name"].str.lower()
-    df["school.locale"] = df["school.locale"].replace([11, 12,13,21,22,23,31,32,33,41,42,43],['Large City', 'Midsize City', 'Small City', 'Large Suburb', 'Midsize Suburb', 'Small Suburb', 'Town Close to a city', 'Distant Town', 'Remote Town', 'Rural but Close to City', 'Distant Rural Area', 'Remote Rural Area'])
-    df['latest.admissions.sat_scores.midpoint.math'] = df['latest.admissions.sat_scores.midpoint.math'].fillna(0)
-    df['latest.admissions.sat_scores.midpoint.critical_reading'] = df['latest.admissions.sat_scores.midpoint.critical_reading'].fillna(0)
-    df['latest.admissions.sat_scores.25th_percentile.math'] = df['latest.admissions.sat_scores.25th_percentile.math'].fillna(0)
-    df['latest.admissions.sat_scores.25th_percentile.critical_reading'] = df['latest.admissions.sat_scores.25th_percentile.critical_reading'].fillna(0)
-    df['latest.admissions.sat_scores.75th_percentile.math'] = df['latest.admissions.sat_scores.75th_percentile.math'].fillna(0)
-    df['latest.admissions.sat_scores.75th_percentile.critical_reading'] = df['latest.admissions.sat_scores.75th_percentile.critical_reading'].fillna(0)
-    df['latest.admissions.act_scores.midpoint.cumulative'] = df['latest.admissions.act_scores.midpoint.cumulative'].fillna(0)
-    df['latest.admissions.act_scores.25th_percentile.cumulative'] = df['latest.admissions.act_scores.25th_percentile.cumulative'].fillna(0)
-    df['latest.admissions.act_scores.75th_percentile.cumulative'] = df['latest.admissions.act_scores.75th_percentile.cumulative'].fillna(0)
-    df['latest.admissions.admission_rate.overall'] = df['latest.admissions.admission_rate.overall'].fillna(0)
-    return df
-''''''    
-
-''''''
-    print(val)
-    median_math_sat = int(df['latest.admissions.sat_scores.midpoint.math'][df[df['school.name'] == val].index[0]])
-    median_verbal_sat = int(df['latest.admissions.sat_scores.midpoint.critical_reading'][df[df['school.name'] == val].index[0]])
-    lower_math = int(df['latest.admissions.sat_scores.25th_percentile.math'][df[df['school.name'] == val].index[0]])
-    lower_verbal = int(df['latest.admissions.sat_scores.25th_percentile.critical_reading'][df[df['school.name'] == val].index[0]])
-    upper_math = int(df['latest.admissions.sat_scores.75th_percentile.math'][df[df['school.name'] == val].index[0]])
-    upper_verbal = int(df['latest.admissions.sat_scores.75th_percentile.critical_reading'][df[df['school.name'] == val].index[0]])
-    median_act = int(df['latest.admissions.act_scores.midpoint.cumulative'][df[df['school.name'] == val].index[0]])
-    lower_act = int(df['latest.admissions.act_scores.25th_percentile.cumulative'][df[df['school.name'] == val].index[0]])
-    upper_act = int(df['latest.admissions.act_scores.75th_percentile.cumulative'][df[df['school.name'] == val].index[0]])
-    percent_white = "{:.2%}".format(df['latest.student.demographics.race_ethnicity.white'][df[df['school.name'] == val].index[0]], "%")
-    percent_black =  "{:.2%}".format(df['latest.student.demographics.race_ethnicity.black'][df[df['school.name'] == val].index[0]], "%")
-    percent_hispanic = "{:.2%}".format(df['latest.student.demographics.race_ethnicity.hispanic'][df[df['school.name'] == val].index[0]], "%")
-    percent_asian = "{:.2%}".format(df['latest.student.demographics.race_ethnicity.asian'][df[df['school.name'] == val].index[0]], "%")
-'''
-
-''''
-if __name__== "__main__":
-    print("Admissions Info:")
-    if df['latest.admissions.admission_rate.overall'][df[df['school.name'] == val].index[0]] !=0:
-        print("Acceptance rate:", "{:.2%}".format(df['latest.admissions.admission_rate.overall'][df[df['school.name'] == val].index[0]], "%"))
-    else: 
-        print("The college you selected did not report admissions rate")
-    if df['latest.admissions.sat_scores.midpoint.critical_reading'][df[df['school.name'] == val].index[0]] != 0:
-        print("Average SAT:", median_math_sat+median_verbal_sat, "range: ", lower_math+lower_verbal,"-",upper_math+upper_verbal)
-        print("Average Math SAT:", median_math_sat, "range:",lower_math,"-",upper_math)
-        print("Average Verbal SAT:", median_verbal_sat, "range:",lower_verbal,"-",upper_verbal)
-    else: 
-        print("The college you selected did not report SAT scores")
-    if df['latest.admissions.act_scores.midpoint.cumulative'][df[df['school.name'] == val].index[0]] != 0:
-        print("Average ACT:", median_act, "range:", lower_act, "-", upper_act)
-    else: 
-        print("The college you selected did not report ACT scores")
-    print("Student Experience:")
-    print("Retention rate:", "{:.2%}".format(df['latest.student.retention_rate.four_year.full_time'][df[df['school.name'] == val].index[0]]))
-    print("Undergraduate Enrollment:", int(df['latest.student.size'][df[df['school.name'] == val].index[0]]), "students")
-    print("Location:", df['school.locale'][df[df['school.name'] == val].index[0]])
-    print("Student Demographics:", percent_white, "White,", percent_black, "Black,", percent_hispanic, "Hispanic,", percent_asian, "Asian")
-    print("Tuition:", "In-State Tuition -", "${:,.2f}".format(df['latest.cost.tuition.in_state'][df[df['school.name'] == val].index[0]]), ",", "Out-Of-State Tuition-", "${:,.2f}".format(df['latest.cost.tuition.out_of_state'][df[df['school.name'] == val].index[0]]))
-    print("Student Success:")
-    print("4-year graduation rate:", "{:.2%}".format(df['latest.completion.completion_rate_4yr_100nt'][df[df['school.name'] == val].index[0]]))
-    print("Average salary after 6 years of entry:", "${:,.2f}".format(df['latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings'][df[df['school.name'] == val].index[0]]))
-    print("Average salary after 8 years of entry:", "${:,.2f}".format(df['latest.earnings.8_yrs_after_entry.mean_earnings'][df[df['school.name'] == val].index[0]]))
-    print("Average salary after 10 years of entry:", "${:,.2f}".format(df['latest.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings'][df[df['school.name'] == val].index[0]]))
-
-'''
